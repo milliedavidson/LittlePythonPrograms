ff8a0abc263edf8ada36117e5e777a88e0f52681
Millie Davidson <milliedavidson@hotmail.com>
2023-04-23 08:02:22 +0100

    Initial commit

---
+++ OutstandingMortgageCalculator.py
@@ -0,0 +1,22 @@
+# Outstanding mortgage calculator
+
+# Mortgage amount left, as a positive number
+mortgage = float(input("What is the mortgage amount left to pay? "))
+
+# Monthly mortgage payment, as a positive integer
+payment = float(input("What is the monthly mortgage payment? "))
+
+# Annual interest rate, as a floating-point percentage
+while True:
+    rate = input("What is the annual interest rate on the mortgage? ")
+    try:
+        rate = float(rate) / 100  # divide by 100 to convert percentage to decimal
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number for the interest rate.")
+
+# Calculate outstanding amount
+interest = mortgage * (rate / 12)  # monthly interest rate
+outstanding = mortgage + interest - payment
+
+print("The outstanding mortgage is: {}.".format(outstanding))
---
+++ README.md
@@ -0,0 +1,13 @@
+# Little Python Programs
+
+A collection of little Python programs, created to practise the language.
+
+<br>
+
+## Programs
+
+[Outstanding Mortgage Calculator](https://github.com/milliedavidson/LittlePythonPrograms/blob/main/OutstandingMortgageCalculator.py)
+
+<br>
+
+<img src ="https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif"/>
